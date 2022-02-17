a23fec7083e049a307c4abb9b7a120a36fb32914
Prasanna <prasannaps2610@gmail.com>
2022-02-17 14:30:35 +0530

    Separate room events handling

--- src/SocketEventsHandler.js
+++ src/SocketEventsHandler.js
@@ -3,25 +3,34 @@ const { createAndJoinNewRoom, joinRoom } = require('./RoomHandler');
 function addSocketEvents(socket, io) {
   /************** Room entry events **************/
   socket.on('create-room', (noOfPlayers = 4) => {
-    const result = createAndJoinNewRoom(noOfPlayers);
-    socket.emit('gameDetails', result);
+    let result = null;
+    try {
+      result = createAndJoinNewRoom(noOfPlayers);
+      setRoomEvents(socket, result.roomId);
+      socket.emit('gameDetails', result);
+    } catch (e) {
+      // TODO :: Make the error message more specific to content
+      result = 'Unable to find room id';
+    }
   });
 
   socket.on('join-room', (roomId, jwt) => {
     let result = null;
     try {
       result = joinRoom(roomId, jwt);
+      setRoomEvents(socket, result.roomId);
+      socket.emit('gameDetails', result);
     } catch (e) {
       // TODO :: Make the error message more specific to content
       result = 'Unable to find room id';
-    } finally {
-      socket.emit('gameDetails', result);
     }
   });
+}
 
+function setRoomEvents(socket, roomId) {
   /************** Game events **************/
   socket.on('m', message => {
-    socket.broadcast.emit('m', message);
+    socket.to(roomId).emit('m', message);
   });
 }
 
